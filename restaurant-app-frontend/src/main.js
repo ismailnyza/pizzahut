3e58eb045f8ae5028a864f27fd9067e3457ee685
Ismail Nyzar <53538980+ismailnyza@users.noreply.github.com>
2021-03-23 15:58:26 +0530

    added key clock login

--- restaurant-app-frontend/src/main.js
+++ restaurant-app-frontend/src/main.js
@@ -1,33 +1,82 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import 'es6-promise/auto'
+import Vue from "vue";
+import Vuex from "vuex";
+import "es6-promise/auto";
 
-import App from './App.vue'
-import router from './router'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import vuetify from './plugins/vuetify';
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import '@mdi/font/css/materialdesignicons.css'
-import axios from 'axios';
-import store from '../src/Store.js'
+import App from "./App.vue";
+import router from "./router";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import vuetify from "./plugins/vuetify";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "@mdi/font/css/materialdesignicons.css";
+import axios from "axios";
+import store from "../src/Store.js";
+import Keycloak from "keycloak-js";
 
 // third party addons
 
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 // state management
-Vue.use(Vuex , axios )
+Vue.use(Vuex, axios );
 
-fetch('/')
+fetch("/");
 
-new Vue({
-  
-  el: '#app',
-  store , 
-  router,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+// adding key cloak
 
+let initOptions = {
+  url: "http://127.0.0.1:8180/auth",
+  realm: "pizzahutapp",
+  clientId: "pizzahutkey",
+  onLoad: "login-required",
+};
+
+let keycloak = Keycloak(initOptions);
+
+keycloak
+  .init({ onLoad: initOptions.onLoad })
+  .then((auth) => {
+    if (!auth) {
+      window.location.reload();
+    } else {
+      // Vue.$log.info("Authenticated");
+
+      new Vue({
+        el: "#app",
+        store,
+        router,
+        vuetify,
+        render: (h) => h(App,
+          //  { props: { keycloak: keycloak } 
+        // }
+          ),
+      });
+    }
+
+    //Token Refresh
+  //   setInterval(() => {
+  //     keycloak
+  //       .updateToken(70)
+  //       .then((refreshed) => {
+  //         if (refreshed) {
+  //           Vue.$log.info("Token refreshed" + refreshed);
+  //         } else {
+  //           Vue.$log.warn(
+  //             "Token not refreshed, valid for " +
+  //               Math.round(
+  //                 keycloak.tokenParsed.exp +
+  //                   keycloak.timeSkew -
+  //                   new Date().getTime() / 1000
+  //               ) +
+  //               " seconds"
+  //           );
+  //         }
+  //       })
+  //       .catch(() => {
+  //         Vue.$log.error("Failed to refresh token");
+  //       });
+  //   }, 6000);
+  // })
+  // .catch(() => {
+  //   Vue.$log.error("Authenticated Failed");
+  });
